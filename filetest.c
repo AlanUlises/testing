34ad1e6932a711f84fb1394257c02ba51a49dfc6
patchouli <patchouli@debian.debian>
2017-03-05 22:59:47 -0600

    Added filetest.c. This program parses a text files and retrieves integer values found.

---
+++ filetest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	FILE * file = fopen("values.txt", "r");
+	const char * token;
+	char line[20];
+
+	while(fgets(line, 20, file)){
+		for(token = strtok(line, ":"); token && token; token = strtok(NULL, ":\n")){
+			printf("%s\n", token);
+		}
+	}
+
+	fclose(file);
+
+	return 0;
+}
